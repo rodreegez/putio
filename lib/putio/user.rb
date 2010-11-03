b2a9648419bdbeebebfb24e356929d4dee1d4321
Adam <electronicbattleweapon7@gmail.com>
2010-11-03 19:19:44 +0000

    move @klass into each method, which is stoopid

--- lib/putio/user.rb
+++ lib/putio/user.rb
@@ -1,8 +1,14 @@
 module User
-  @klass  = 'user'
 
   def get_user_info
+    @klass  = 'user'
     @action = 'info'
+    request
+  end
+
+  def get_user_friends
+    @klass  = 'user'
+    @action = 'friends'
     request
   end
 end
