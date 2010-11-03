75d38578d587a91a3c6ff54d35cc5b46a69023f9
Adam <electronicbattleweapon7@gmail.com>
2010-11-03 19:21:17 +0000

    correct expected url

--- test/units/user_test.rb
+++ test/units/user_test.rb
@@ -4,9 +4,9 @@ class UserTest < Test::Unit::TestCase
   context 'A Putio client with valid credentials' do
     setup { @putio = Putio.new('abc', '123') }
     should 'return user\'s info' do
-      stub(%Q|http://api.put.io/v1/?method=info&request=|, 'user_info.json')
+      stub(%Q|http://api.put.io/v1/user?method=info&request=|, 'user_info.json')
       user_info = @putio.get_user_info
-      
+
       assert_equal %Q{{"test":"data"}}, user_info.chomp
     end
   end
